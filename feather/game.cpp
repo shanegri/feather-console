7685b5afe40a0e7bcd165175625e57b132b60cf3
Nick Kobis <nickkobis@gmail.com>
2018-11-27 15:36:01 -0500

    added deflection

--- feather/game.cpp
+++ feather/game.cpp
@@ -4,15 +4,15 @@
 
 //All game code goes here
 
-uint8_t p1 = 0;
-uint8_t p2 = 25;
-uint8_t bx = 10;
-uint8_t by = 10;
+int p1 = 0;
+int p2 = 25;
+int bx = 10;
+int by = 10;
 
 int pos_velocity_x = 32768; // (1 << 15)
 int pos_velocity_y = 32768;
 int neg_velocity_x = 32768; // (1 << 15)
-int neg_velocity_y = 32768;
+int neg_velocity_y = 32768;// subtract negative velocities
 
 uint8_t velocity_flag = 3; //(xxxx xx(x)(y)
 //0 is neg x, neg y
@@ -28,21 +28,21 @@ uint8_t button_status = 0;
 
 
 void update_movement(){
-  if((button_status & (1 << 3)) && p1 > -3){
+  if((button_status & (1 << 3)) && p1 >= 0){
     p1 = p1 - 1;
   }
-  if((button_status & (1 << 1)) && p1 < 29 ){
+  if((button_status & (1 << 1)) && p1 < B_HEIGHT - P_HEIGHT ){
     p1 = p1 +1;
   }
-  if((button_status & (1 << 2)) && p2 > -3){
+  if((button_status & (1 << 2)) && p2 >= 0){
     p2 = p2-1;
   }
-  if((button_status & 1) && p2 < 29){
+  if((button_status & 1) && p2 < P_HEIGHT - P_OFFSET){
     p2 = p2 +1;
   }
 }
 
-
+int tempx;
 void update_ball_position(){
   switch(velocity_flag){
     case 0:
@@ -62,11 +62,23 @@ void update_ball_position(){
       real_by = real_by + pos_velocity_y;
       break;
   }
+  tempx = bx;
   bx = real_bx >> 16;
   by = real_by >> 16;
+  //check deflection
+  if(tempx >= P_OFFSET && bx <= P_OFFSET && by>= p1 && by <= p1+P_HEIGHT ){
+    run_deflection(true);
+    return;
+  }
+  if(tempx >= B_WIDTH - P_OFFSET && bx <= B_WIDTH && by>= p2 && by <= p2+P_HEIGHT ){
+    run_deflection(false);
+    return;
+  }
   
 }
-
+void run_deflection(bool is_player1){
+  velocity_flag ^= 2;
+}
 void update_ball_velocity(){
   if(by < 0 || by >30){
     velocity_flag ^= 1;
